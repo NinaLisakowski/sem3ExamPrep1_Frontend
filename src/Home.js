6a31c98dee39136b4599017555d055902bc8f937
NinaLisakowski <til.nina@hotmail.com>
2020-06-10 15:28:35 +0200

    first

---
+++ src/Home.js
@@ -0,0 +1,38 @@
+import React from "react";
+
+export function Home() {
+	return (
+		<div>
+			<h2>Home</h2>
+			<p>
+				Make sure to follow both instructions for the frontend and the backend
+				:)
+			</p>
+
+			<p>
+				Link to backend github:{" "}
+				<a href="https://github.com/Gold-ish/StartCode_CA3_Backend">
+					Backend setup
+				</a>
+			</p>
+			<p>
+				Link to frontend github:{" "}
+				<a href="https://github.com/Gold-ish/StartCode_CA3_Frontend">
+					Frontend setup
+				</a>
+			</p>
+			<br />
+			<p>
+				To login as user or admin, use the following usernames and password:
+			</p>
+			<p>Username: admin, password: code</p>
+			<p>Username: user1, password: code1</p>
+			<p>Username: user2, password: code2</p>
+			<p>Username: user3, password: code3</p>
+			<p>
+				<b>This is Ninas personal startcode project</b>
+				<br />I followed the instructions from the starcode.
+			</p>
+		</div>
+	);
+}
